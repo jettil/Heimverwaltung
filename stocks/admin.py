a5bc73af92f3097400742a6fcf68fc8cd8162485
TIll <till-mueller@gmx.net>
2021-02-27 13:39:23 +0100

    Added models

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,5 +1,7 @@
 # Register your models here
 from django.contrib import admin
-from .models import  Stammdaten
+from .models import Stammdaten
+from .models import Lagerinhalt
 
 admin.site.register(Stammdaten)
+admin.site.register(Lagerinhalt)
--- stocks/models.py
+++ stocks/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Stammdaten(models.Model):
-    product_name = models.CharField(max_length=250)
-    temperature = models.IntegerField
+    product_name = models.CharField(max_length=250) #Name des Produkts
+    unit = models.CharField(max_length=100, default="")#Einheit mit der das Produkt berechnet wird
+    storage_temperature = models.IntegerField(default=20)#Temperatur bei das Produkt VOR dem Öffnen gelagert wird
+    storage_temperature_open = models.IntegerField(default=20)#Temperatur bei das Produkt NACH dem Öffnen gelagert wird
+    durability = models.IntegerField(default=720)#Übliche Haltbarkeit nach dem Kaufen in Tagen default sind zwei Jahre
+    durability_open = models.IntegerField(default=720)#Ubliche Haltbarkeit nach dem öffnen, default sind zwei Jahre
+
+
+class Lagerinhalt(models.Model):
+    product = models.ForeignKey(Stammdaten, on_delete=models.CASCADE)#Link zu den Stammdaten
+    quantity = models.IntegerField(default=1)#Menge des Produktes in der entsprechenden Einheit
+    day_purchased = models.DateField('date purchased') #Tag des Kaufs
+    day_opened = models.DateField('date opened')#Tag der Öffnung
